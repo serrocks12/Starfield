d2b2ebe1e25e16893645df9a3ab4b769a9f5acbe
sebenchohra <sbenchohra12@gmail.com>
2014-10-23 11:14:09 -0700

    Added first name to index.html

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle [] myParticle = new Particle[50]; //your code here
-void setup()
+public void setup()
 {
 	background(0);
 	size(500,500);
@@ -9,7 +25,7 @@ void setup()
 	}
 	myParticle[0] = new OddballParticle(250,250);
 }
-void draw()
+public void draw()
 {
 	for( int nI = 0; nI < 50 ; nI++)
 	{
@@ -72,3 +88,12 @@ class OddballParticle implements Particle
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
